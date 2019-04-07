dd941b3e8b0c879752fb76d29309f174805ac619
Edgars <edgars.avenins@gmail.com>
2019-04-07 18:24:51 +1200

    some functionality

--- training-easy/src/app/calculator/calculator.component.ts
+++ training-easy/src/app/calculator/calculator.component.ts
@@ -15,43 +15,25 @@ export interface Tile {
 export class CalculatorComponent implements OnInit {
 
   tiles: Tile[] = [
-    { text: '%', cols: 1, rows: 1, color: 'lightblue' },
-    { text: 'Sqrt', cols: 1, rows: 1, color: 'lightblue' },
-    { text: 'x^2', cols: 1, rows: 1, color: 'lightblue' },
-    { text: '1/x', cols: 1, rows: 1, color: 'lightblue' },
-
-    { text: 'CE', cols: 1, rows: 1, color: 'lightblue' },
-    { text: 'C', cols: 1, rows: 1, color: 'lightblue' },
-    { text: 'DEL', cols: 1, rows: 1, color: 'lightblue' },
-    { text: '/', cols: 1, rows: 1, color: 'lightblue' },
-
-    { text: '7', cols: 1, rows: 1, color: 'lightblue' },
-    { text: '8', cols: 1, rows: 1, color: 'lightblue' },
-    { text: '9', cols: 1, rows: 1, color: 'lightblue' },
-    { text: 'X', cols: 1, rows: 1, color: 'lightblue' },
-
-    { text: '4', cols: 1, rows: 1, color: 'lightblue' },
-    { text: '5', cols: 1, rows: 1, color: 'lightblue' },
-    { text: '6', cols: 1, rows: 1, color: 'lightblue' },
-    { text: '-', cols: 1, rows: 1, color: 'lightblue' },
-
-    { text: '1', cols: 1, rows: 1, color: 'lightblue' },
-    { text: '2', cols: 1, rows: 1, color: 'lightblue' },
-    { text: '3', cols: 1, rows: 1, color: 'lightblue' },
     { text: '+', cols: 1, rows: 1, color: 'lightblue' },
-
-    { text: '+/-', cols: 1, rows: 1, color: 'lightblue' },
-    { text: '0', cols: 1, rows: 1, color: 'lightblue' },
-    { text: '.', cols: 1, rows: 1, color: 'lightblue' },
-    { text: '=', cols: 1, rows: 1, color: 'lightblue' },
+    { text: '-', cols: 1, rows: 1, color: 'lightblue' },
+    { text: '*', cols: 1, rows: 1, color: 'lightblue' },
+    { text: '/', cols: 1, rows: 1, color: 'lightblue' },
   ];
 
-  inputValue;
+  inputValue: string;
+  sum: number;
 
-  clicked(event) {
-    this.inputValue = event.srcElement.innerText;
-    console.log(event.srcElement.innerText);
+  clicked(operator, value1) {
+    console.log(operator);
+    this.inputValue = value1 + operator ;
   }
+
+  calculate(lastVal) {
+    this.inputValue = lastVal;
+  this.sum = eval(this.inputValue)
+  }
+  
 
   constructor() { }
 

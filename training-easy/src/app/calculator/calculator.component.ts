3ce62cd8ad92c5a802d40a7c997b6ef4645e9ea9
Edgars <edgars.avenins@gmail.com>
2019-04-08 12:13:32 +1200

    more functionality

--- training-easy/src/app/calculator/calculator.component.ts
+++ training-easy/src/app/calculator/calculator.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 export interface Tile {
   color: string;
@@ -23,34 +23,54 @@ export class CalculatorComponent implements OnInit {
     { text: '7', cols: 1, rows: 1, color: 'lightblue' },
     { text: '8', cols: 1, rows: 1, color: 'lightblue' },
     { text: '9', cols: 1, rows: 1, color: 'lightblue' },
-    { text: ' ', cols: 1, rows: 1, color: 'lightblue' },
+    { text: '(', cols: 1, rows: 1, color: 'lightblue' },
 
     { text: '4', cols: 1, rows: 1, color: 'lightblue' },
     { text: '5', cols: 1, rows: 1, color: 'lightblue' },
     { text: '6', cols: 1, rows: 1, color: 'lightblue' },
-    { text: ' ', cols: 1, rows: 1, color: 'lightblue' },
+    { text: ')', cols: 1, rows: 1, color: 'lightblue' },
 
     { text: '1', cols: 1, rows: 1, color: 'lightblue' },
     { text: '2', cols: 1, rows: 1, color: 'lightblue' },
     { text: '3', cols: 1, rows: 1, color: 'lightblue' },
-    { text: ' ', cols: 1, rows: 1, color: 'lightblue' },
+    { text: 'DEL', cols: 1, rows: 1, color: 'lightblue' },
 
-    { text: '0', cols: 4, rows: 1, color: 'lightblue' },
+    { text: '0', cols: 3, rows: 1, color: 'lightblue' },
+    { text: 'C', cols: 1, rows: 1, color: 'lightblue' },
   ];
 
   inputValue: string = '';
   sum: number;
 
   clicked(gridValue) {
-    console.log(gridValue);
-    this.inputValue = this.inputValue + gridValue;
+    if (gridValue == 'C' || gridValue ==  'DEL') {
+        this.calculatorDelete(gridValue);
+    } else {
+        this.inputValue = this.inputValue + gridValue;
+    }
   }
 
-  calculate(lastVal) {
-    this.inputValue = lastVal;
-  this.sum = eval(this.inputValue)
+  calculate() {
+    this.sum = eval(this.inputValue)
   }
   
+  calculatorDelete(operator) {
+    if (operator == 'C') {
+      this.refresh();
+    } else {
+      this.remove();
+    }
+  }
+
+  refresh() {
+    this.inputValue = '';
+  }
+
+  remove() {
+    var strLen = this.inputValue.length;
+    this.inputValue = this.inputValue.slice(0, strLen-1);
+    console.log(this.inputValue);
+  }
 
   constructor() { }
 
